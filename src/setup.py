3d5132653ce28992ee36cf2c2dd51e858bea8cef
Flavio Codeco Coelho <fccoelho@gmail.com>
2010-03-22 16:20:09 +0000

    removed dependency on ez_setup.py

--- src/setup.py
+++ src/setup.py
@@ -5,15 +5,14 @@ Created on 22/03/2010
 @author: fccoelho
 '''
 
-from ez_setup import use_setuptools
-use_setuptools()
-#from distutils.core import setup
-#from distutils.extension import Extension
+#from ez_setup import use_setuptools
+#use_setuptools()
+
 from setuptools import setup, find_packages
 
 
 setup(name='liveplots', 
-        version  = '0.1.0', 
+        version  = '0.1.1', 
         author = 'Flavio Codeco Coelho', 
         author_email = 'fccoelho@gmail.com', 
         url = 'http://code.google.com/p/liveplots/',
