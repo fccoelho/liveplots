77ddf0e6ff5309c4259d8b07bc0f2aaff604ff47
Flavio Codeco Coelho <fccoelho@gmail.com>
2010-04-15 10:48:22 +0100

    started implementation of file monitor

--- src/setup.py
+++ src/setup.py
@@ -12,14 +12,14 @@ from setuptools import setup, find_packages
 
 
 setup(name='liveplots', 
-        version  = '0.2', 
+        version  = '0.3', 
         author = 'Flavio Codeco Coelho', 
         author_email = 'fccoelho@gmail.com', 
         url = 'http://code.google.com/p/liveplots/',
         description = 'Real-time live plot server',
         zip_safe = True,
         packages = find_packages(),
-        install_requires = ["numpy >= 1.2"], 
+        install_requires = ["numpy >= 1.2","pyinotify >= 0.8.9"], 
         test_suite = 'nose.collector', 
         license = 'GPL',  
       )
